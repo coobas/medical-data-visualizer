7c8ee7776c762735c40649d0f1ed0f8083bf3573
Jakub Urban <coobas@gmail.com>
2023-04-30 17:07:11 +0200

    streamlit 3

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ import streamlit as st
 from medical_data_visualizer import load_data, draw_cat_plot, draw_heat_map
 
 
-df = load_data()
+with st.sidebar:
+    bmi_overweight = st.slider("BMI Overweight", 15, 35, 25)
+    split_by = st.selectbox("Split by", ["cardio", "sex", "smoke", "alco", "active", "overweight"])
+    outlier_quantile = st.number_input("Outlier Quantile", 0.0, 0.2, value=0.025, step=0.005, format="%.3f")
+
+df = load_data(bmi_overweight=bmi_overweight)
 
 st.title("Medical Data Visualizer")
 st.header("Value counts")
-st.write(draw_cat_plot(df))
+st.write(draw_cat_plot(df, split_by=split_by))
 
 st.header("Correlation Heat Map")
-st.write(draw_heat_map(df))
+st.write(draw_heat_map(df, outlier_quantile=outlier_quantile))
--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def load_data():
+def load_data(bmi_overweight=25):
     # Import data
     df = pd.read_csv("medical_examination.csv")
 
     # Add 'overweight' column
-    df["overweight"] = (df["weight"] / ((df["height"] / 100) ** 2) > 25).astype(int)
+    df["overweight"] = (df["weight"] / ((df["height"] / 100) ** 2) > bmi_overweight).astype(int)
 
     # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 
@@ -21,17 +21,17 @@ def load_data():
 
 
 # Draw Categorical Plot
-def draw_cat_plot(df):
+def draw_cat_plot(df, split_by="cardio"):
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(
         df,
-        id_vars=["cardio"],
+        id_vars=[split_by],
         value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"],
     )
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
     df_cat = pd.DataFrame(
-        df_cat.groupby(["cardio", "variable", "value"]).size().reset_index(name="total")
+        df_cat.groupby([split_by, "variable", "value"]).size().reset_index(name="total")
     )
 
     # Draw the catplot with 'sns.catplot()'
@@ -40,7 +40,7 @@ def draw_cat_plot(df):
         x="variable",
         y="total",
         hue="value",
-        col="cardio",
+        col=split_by,
         kind="bar",
     )
 
@@ -53,14 +53,14 @@ def draw_cat_plot(df):
 
 
 # Draw Heat Map
-def draw_heat_map(df):
+def draw_heat_map(df, outlier_quantile=0.025):
     # Clean the data
     df_heat = df[
         (df["ap_lo"] <= df["ap_hi"])
-        & (df["height"] >= df["height"].quantile(0.025))
-        & (df["height"] <= df["height"].quantile(0.975))
-        & (df["weight"] >= df["weight"].quantile(0.025))
-        & (df["weight"] <= df["weight"].quantile(0.975))
+        & (df["height"] >= df["height"].quantile(outlier_quantile))
+        & (df["height"] <= df["height"].quantile(1 - outlier_quantile))
+        & (df["weight"] >= df["weight"].quantile(outlier_quantile))
+        & (df["weight"] <= df["weight"].quantile(1 - outlier_quantile))
     ]
 
     # Calculate the correlation matrix
