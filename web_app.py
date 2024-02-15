4c8c725da35c781d8b2f6cc2b8d3853cc77a7ddf
Jakub Urban <coobas@gmail.com>
2024-02-15 15:27:59 +0100

    simple web app

---
+++ web_app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# Load the data
+medical_data = pd.read_csv("medical_examination.csv")
+
+st.title("Medical Data Visualizer")
+
+st.markdown(
+    """
+    This web app visualizes the medical data provided in the CSV file `medical_examination.csv`.
+    """
+)
+st.dataframe(medical_data)
+
+st.header("Histogram visualization")
+
+hist_variable = st.selectbox(
+    "Select a variable to display as histogram", medical_data.columns,
+)
+
+fig = px.histogram(
+    medical_data,
+    x=hist_variable,
+    color="sex",
+    barmode="group",
+    histnorm="percent",
+)
+st.plotly_chart(fig)
